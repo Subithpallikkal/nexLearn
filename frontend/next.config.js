a27bed50d76325f96fae7cd68c605de6982de53c
Subithpallikkal <subithpallikkal17@gmail.com>
2026-04-03 17:34:17 +0530

    update build file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+  output: "standalone"
 };
 
 module.exports = nextConfig;
